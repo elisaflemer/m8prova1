8649571c9c996c1229de98e9299f6a7bedcdd904
Elisa Flemer <elisaflemer@gmail.com>
2023-11-17 15:40:07 -0300

    chatzitos baconzitos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# m8prova1
+# Prova 1 | módulo 8
+
+A atividade desta prova foi criar um chatbot simples em Python que entenda duas intenções de usuário (acompanhar pedido/trocar forma de pagamento) a partir de expressões regulares, e responda de acordo.
+
---
+++ chatbot.py
@@ -0,0 +1,23 @@
+import re
+
+intencoes = {
+    r"(((A|a)tualiz|(M|m)ud|(A|a)lter|(T|t)roc)(?s).*((P|p)ix|(c|C)r(é|e)dito|(C|c)art|(D|d)(é|e)bito|(P|p)ag))": "atualizar_pagamento",
+    r"(status|rastre|(O|o)nde est)": "rastrear"
+}
+
+acoes = {
+        "atualizar_pagamento": "Para modificar sua forma de pagamento, liga para nosso 0800 e prepara-se para uma imersão no inferno burocrático kafkiano!",
+        "rastrear": 'Para rastrear seu pedido, sente na porta de casa e espere o motoqueiro aparecer. Quando isso acontecer, faltará poucos segundos para sua entrega.'
+}
+
+while True:
+    command = input("Você: ")
+    if command == "sair":
+        print("Até mais")
+        exit()
+    for key, value in intencoes.items():
+        pattern = re.compile(key)
+        groups = pattern.findall(command)
+        if groups:
+            print(acoes[value])
+    print()
