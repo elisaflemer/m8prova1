18923108b08519a2dfda8f61db4e9c574e80ac99
Elisa Flemer <elisaflemer@gmail.com>
2023-11-17 15:55:34 -0300

    readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 A atividade desta prova foi criar um chatbot simples em Python que entenda duas intenções de usuário (acompanhar pedido/trocar forma de pagamento) a partir de expressões regulares, e responda de acordo.
 
+As expressões em si procuram por variações de "atualizar", "trocar", "mudar", "pix", "pagamento", "cartão", "débito" e "crédito" para a intenção de atualização de pagamento; e "status", "rastreamento/rastrear/rastreio", "onde está" para acompanhamento do pedido.
+
+P.S: por frescurice de última hora do prof. Murilo, o chat tb fala oi.
--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,15 @@
 import re
 
 intencoes = {
-    r"(((A|a)tualiz|(M|m)ud|(A|a)lter|(T|t)roc)(?s).*((P|p)ix|(c|C)r(é|e)dito|(C|c)art|(D|d)(é|e)bito|(P|p)ag))": "atualizar_pagamento",
-    r"(status|rastre|(O|o)nde est)": "rastrear"
+    r"(((A|a)tualiz|(M|m)ud|(A|a)lter|(T|t)roc)(?s).*((P|p)ix|(c|C)r(é|e)dito|(C|c)art|(D|d)(é|e)bito|(P|p)ag)|(desatualiz))": "atualizar_pagamento",
+    r"(status|rastre|(O|o)nde est)": "rastrear",
+    r"((O|o)(i|i(ê|E)|ii|l(a|á)))": "oi"
 }
 
 acoes = {
         "atualizar_pagamento": "Para modificar sua forma de pagamento, liga para nosso 0800 e prepara-se para uma imersão no inferno burocrático kafkiano!",
-        "rastrear": 'Para rastrear seu pedido, sente na porta de casa e espere o motoqueiro aparecer. Quando isso acontecer, faltará poucos segundos para sua entrega.'
+        "rastrear": 'Para rastrear seu pedido, sente na porta de casa e espere o motoqueiro aparecer. Quando isso acontecer, faltará poucos segundos para sua entrega.',
+        "oi": "Olá <3"
 }
 
 while True:
